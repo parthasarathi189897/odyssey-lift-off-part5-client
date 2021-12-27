ab1e264331f283d82f01a483e0a07b708966f398
Partha <parthasarathi.das@walmart.com>
2021-12-27 15:42:07 -0800

    modify url

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: 'https://server-catstronauts-gql-poc.herokuapp.com/',
   cache: new InMemoryCache(),
 });
 
